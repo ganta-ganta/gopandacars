c2cbb44ce9c40c7789299d4d2f47425f4672f8fe
ganta-ganta <gantasolutions@gmail.com>
2024-10-25 01:49:50 +0530

    Update script1.js

--- rk/script1.js
+++ rk/script1.js
@@ -2,3 +2,17 @@ function dismissBanner() {
   const banner = document.getElementById('important-note-banner');
   banner.style.display = 'none';
 }
+// Function to open the banner
+function openBanner() {
+  document.getElementById('diwaliBanner').style.display = 'flex';
+}
+
+// Function to close the banner
+function closeBanner() {
+  document.getElementById('diwaliBanner').style.display = 'none';
+}
+
+// Automatically open the banner when the page loads
+window.onload = function() {
+  setTimeout(openBanner, 1000); // Opens the banner 1 second after the page loads
+}
